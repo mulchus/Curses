351ab07f8cb3cf4063845c2743a2d526941d3397
mulchus <mulch@list.ru>
2023-06-08 16:49:55 +0300

    some coroutine edits.

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import curses
 import asyncio
 import random
 import curses_tools
+
 from itertools import cycle
+
 
 TIC_TIMEOUT = 0.1
 
 
 def draw(canvas):
-    canvas.border(0 * 8)
+    canvas.border(0)
     canvas.refresh()
 
     (max_row, max_column) = canvas.getmaxyx()
@@ -17,10 +19,14 @@ def draw(canvas):
     numbers_of_stars = int(max_row * max_column / 40)
     coroutines = []
     for _ in range(numbers_of_stars):
-        coroutines.append(blink(canvas,\
-                        random.randint(2, max_row - 2),\
-                        random.randint(2, max_column - 2),\
-                        random.choice('+*.:')))
+        coroutines.append(
+            blink(
+                canvas,
+                random.randint(2, max_row - 2),
+                random.randint(2, max_column - 2),
+                random.choice('+*.:')
+            )
+        )
 
     rocket_row = max_row / 2
     rocket_column = max_column / 2
@@ -30,9 +36,13 @@ def draw(canvas):
     with open("Animations/rocket_frame_2.txt", "r") as my_file:
         rocket_frame.append(my_file.read())
 
-    rocket_coroutine = animate_spaceship(canvas, rocket_row,\
-                                         rocket_column,\
-                                         rocket_frame)
+    rocket_coroutine = animate_spaceship(
+        canvas,
+        rocket_row,
+        rocket_column,
+        rocket_frame
+    )
+    coroutines.append(rocket_coroutine)
 
     rows_direction = 0
     columns_direction = 0
@@ -43,12 +53,10 @@ def draw(canvas):
 
         try:
             canvas.nodelay(True)
-            rows_direction, columns_direction, space_pressed =\
-                curses_tools.read_controls(canvas)
+            rows_direction, columns_direction, space_pressed = curses_tools.read_controls(canvas)
 
             if rows_direction:
-                if (rocket_row + rows_direction) > 0 and (
-                        rocket_row + rows_direction) < (max_row - 9):
+                if 0 < (rocket_row + rows_direction) < (max_row - 9):
                     rocket_row += rows_direction
                 elif (rocket_row + rows_direction) <= 0:
                     rocket_row = 1
@@ -69,39 +77,49 @@ def draw(canvas):
             if space_pressed:
                 fire_coroutine = fire(canvas, rocket_row, rocket_column + 2,
                                       -1, 0)
+                coroutines.append(fire_coroutine)
                 space_pressed = False
         except StopIteration:
             pass
         canvas.nodelay(False)
 
-        for _ in range(len(coroutines.copy())):
-            coroutine = coroutines.copy()[random.randint(0,\
-                                        len(coroutines.copy())-1)]
+        # print(len(coroutines.copy()))
+
+        for i in range(len(coroutines.copy())):
+            coroutine = coroutines.copy()[random.randint(0,
+                                          len(coroutines.copy())-1)]
+
             try:
                 coroutine.send(None)
+                canvas.refresh()
             except StopIteration:
+                print(i, coroutine)
                 coroutines.remove(coroutine)
+
             if not len(coroutines):
                 break
-        canvas.refresh()
 
-        try:
-            rocket_coroutine.send(None)
-            canvas.refresh()
-        except StopIteration:
-            rocket_coroutine = animate_spaceship(canvas,\
-                                        rocket_row,\
-                                        rocket_column,\
-                                        rocket_frame)
-            pass
+        # canvas.refresh()
 
-        try:
-            if fire_coroutine:
-                fire_coroutine.send(None)
-                canvas.refresh()
-        except StopIteration:
-            fire_coroutine = 0
-            pass
+        # try:
+        #     rocket_coroutine.send(None)
+        #     canvas.refresh()
+        # except StopIteration:
+        #     rocket_coroutine = animate_spaceship(
+        #         canvas,
+        #         rocket_row,
+        #         rocket_column,
+        #         rocket_frame
+        #     )
+        #     pass
+
+        # try:
+        #     if fire_coroutine:
+        #         fire_coroutine.send(None)
+        #         canvas.refresh()
+        # except StopIteration:
+        #     fire_coroutine = 0
+        #     pass
 
         # canvas.refresh()
         time.sleep(TIC_TIMEOUT)
@@ -132,31 +150,46 @@ async def animate_spaceship(canvas, start_row, start_column, rocket_frames):
 
     rocket = next(iterator)
 
-    curses_tools.draw_frame(canvas, round(row), round(column),\
-                            rocket)
+    curses_tools.draw_frame(
+        canvas,
+        round(row),
+        round(column),
+        rocket)
+
     for _ in range(0, 2):
         await asyncio.sleep(0)
 
-    curses_tools.draw_frame(canvas, round(row), round(column),\
-                            rocket, negative=True)
+    curses_tools.draw_frame(
+        canvas,
+        round(row),
+        round(column),
+        rocket,
+        negative=True)
+
     await asyncio.sleep(0)
 
     rocket = next(iterator)
 
-    curses_tools.draw_frame(canvas, round(row), round(column),\
-                            rocket)
+    curses_tools.draw_frame(
+        canvas,
+        round(row),
+        round(column),
+        rocket)
+
     for _ in range(0, 2):
         await asyncio.sleep(0)
-    curses_tools.draw_frame(canvas, round(row), round(column),\
-                            rocket, negative=True)
+
+    curses_tools.draw_frame(
+        canvas,
+        round(row),
+        round(column),
+        rocket,
+        negative=True)
+
     await asyncio.sleep(0)
 
 
-async def fire(canvas,
-               start_row,
-               start_column,
-               rows_speed=-0.3,
-               columns_speed=0):
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
     """Display animation of gun shot, direction and speed can be specified."""
 
     row, column = start_row, start_column
